99abf7d9eb44150254f72c95db4cafe65516d4ef
Bere <berecast1604@gmail.com>
2026-05-25 12:54:42 -0600

    Cambio de granulidad
    granulidad actual fecha + anuncio + edad + genero

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ META_FIELDS = os.getenv(
     "date_start,date_stop,campaign_id,campaign_name,adset_id,adset_name,ad_id,ad_name,objective,impressions,reach,frequency,clicks,inline_link_clicks,cpc,cpm,ctr,spend,actions,cost_per_action_type"
 )
 
-META_BREAKDOWNS = os.getenv("META_BREAKDOWNS", "")
+META_BREAKDOWNS = os.getenv("META_BREAKDOWNS", "age,gender")
 META_LEVEL = os.getenv("META_LEVEL", "ad")
 META_TIME_INCREMENT = os.getenv("META_TIME_INCREMENT", "1")
 META_DATE_PRESET = os.getenv("META_DATE_PRESET", "yesterday")
